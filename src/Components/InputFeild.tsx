e13deb6d32624366a3df72d053993c5e8129165e
dhungmina <minadhungana901@gmail.com>
2023-04-01 00:21:49 +0545

    fetched the list

--- src/Components/InputFeild.tsx
+++ src/Components/InputFeild.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './styles.css'
-const InputFeild = () => {
+interface Props{
+    todo:string;
+    setTodo:React.Dispatch<React.SetStateAction<string>>
+    handleAdd: (e:React.FormEvent)=>void;
+  }
+  
+const InputFeild = ({todo,setTodo,handleAdd}:Props) => {
   return (
-    <form className="input">
-        <input type="input" placeholder='Enter a task' className='inputbox'/>
+    <form className="input" onSubmit={(e)=>handleAdd(e)}>
+        <input type="input" placeholder='Enter a task' className='inputbox' value={todo} onChange={(e)=>{setTodo(e.target.value)}}/>
         <button className='inputsub' type='submit'>Go</button>
     </form>
   )
